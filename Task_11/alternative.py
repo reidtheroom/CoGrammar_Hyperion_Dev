f05fce81c619ff3f253e164422d9ad13b69c0e65
reidtheroom <sjkhanom04@gmail.com>
2024-01-15 09:27:45 +0000

    mentored task 12

--- Task_11/alternative.py
+++ Task_11/alternative.py
@@ -2,19 +2,28 @@ string = "my name is jubaydah, and i am the best in the world."
 
 string_alternate = ''
 
-# for x in range(len(string)):
-#     if x % 2 == 0:
-#         string_alternate += string[x].lower()
-#     else:
-#         string_alternate += string[x].upper()
-# print(string_alternate)
+# switching between each letter/index value
+
+for x in range(len(string)):
+    if x % 2 == 0:
+        string_alternate += string[x].lower()
+    else:
+        string_alternate += string[x].upper()
+print(string_alternate)
+
+# switching between each word
 
 seperate_string = string.split()
 
 alternate = []
 
-print(seperate_string)
+string_alternate = ''
 
-# alternate1 = alternate.join(' ')
+for x in range(len(seperate_string)):
+    if x % 2 == 0:
+        alternate.append(seperate_string[x].lower())
+    else:
+        alternate.append(seperate_string[x].upper())
 
-# print(alternate)
+string_alternate = ' '.join(alternate)
+print(string_alternate)
